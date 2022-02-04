f39c0bb4a8a931e2a93344eb0808511068638557
jerge <kayir74@gmail.com>
2022-02-04 14:43:15 +0100

    get all examples

--- model/gym_builderarch/envs/builderarch_env.py
+++ model/gym_builderarch/envs/builderarch_env.py
@@ -50,8 +50,9 @@ class BuilderArchEnv(gym.Env):
         self.state = torch.zeros(self.size)
         self.loc = 0
         self.steps = 0
-        #ex = random.choice(get_easy_examples7()[1])
-        ex = get_easy_examples7()[4][1]
+        # Examples are of form (name,grid)
+        ex = random.choice(get_easy_examples7()[1])
+        #ex = get_easy_examples7()[4][1]
         self.set_goal(ex)
         return self.get_state()
 
